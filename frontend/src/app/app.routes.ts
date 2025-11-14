1fa00e888051d2698e5552140f8bf3140dda4a92
Lochana Marasinghe <lochanatharanganie@gmail.com>
2025-11-13 16:19:41 -0800

    added vegetation indices component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {ViewImagesComponent} from "./view/view-images/view-images.component";
+import {VegetationIndicesComponent} from "./view/vegetation-indices/vegetation-indices.component";
 
 export const routes: Routes = [
   { path: '', component: ViewImagesComponent },
-  { path: 'view-images', component: ViewImagesComponent }
+  { path: 'view-images', component: ViewImagesComponent },
+  { path: 'vegetation-indices', component: VegetationIndicesComponent },
 ];
---
+++ frontend/src/app/view/vegetation-indices/vegetation-indices.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vegetation-indices',
+  standalone: true,
+  imports: [],
+  templateUrl: './vegetation-indices.component.html',
+  styleUrl: './vegetation-indices.component.css'
+})
+export class VegetationIndicesComponent {
+
+}
